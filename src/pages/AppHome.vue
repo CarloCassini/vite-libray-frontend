4ccf59d76771b52e41ca731c7e67909743d4158f
CarloCassini <c.cassini87@gmail.com>
2023-11-10 16:15:24 +0100

    add detail page

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1 class="container my-5">home</h1>
+  <router-link :to="{ name: 'detail' }"> vai al dettaglio</router-link>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/pages/Detail.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <h1 class="container my-5">
+    BECCATI STO TRAPEZIO PER PIACEREEEEEEEEEEEEEEEEE
+  </h1>
+</template>
+
+<style lang="scss" scoped></style>
